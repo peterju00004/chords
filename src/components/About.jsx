361848e8bd5c4a01cf7dc5086c0f207343d6712c
Peter Ju <peterju00004@gmail.com>
2025-12-28 19:57:52 -0500

    polished about page style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,21 +9,9 @@ const About = () => {
                 <br /><br />
                 To Use Chords, connect your MIDI device to your computer and navigate to the Devices page to select your MIDI device. Once you have selected your MIDI device, navigate to the Home page to start playing notes on your MIDI device. Chords will detect the notes you play and display the chord you are playing.
                 <br /><br />
-                To install Chords on your computer, follow the instructions in the README file on the GitHub repository.
-                <br /><br />
-                Chords supported: <br />
-                - Major (6th/7th/9th)<br />
-                - Minor (6th/7th/9th)<br />
-                - Augmented (7th)<br />
-                - Diminished (7th)<br />
-                - Dominant 7th/9th <br />
-                - Half Diminished 7th <br />
-                - Major 7th Sharp 5th <br />
-                - sus2 <br />
-                - sus4 <br />
-                - add2 <br />
-
-                <br /><br />
+                To install Chords on your computer, follow the instructions in the README file in the GitHub repository.
+                <br />
+                <br />
                 My personal website: <a href="https://finesite.org" target="_blank" rel="noreferrer">https://finesite.org</a> <br/>
                 If you have any suggestions, questions, or feedback, please feel free to reach out to me.
             </p>
